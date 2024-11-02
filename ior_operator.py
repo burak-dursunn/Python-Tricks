2198a3666090ce3049a5e7b423d8cb8983d1905a
Burak Dursun <dursunburak046@gmail.com>
2024-11-03 02:02:16 +0300

    Update ior_operator.py

--- ior_operator.py
+++ ior_operator.py
@@ -5,5 +5,5 @@ print(a | b)
 a |= b
 print(a)
 print("Burak" != "Burak")
-# IOR operaörü list ve tuple gibi veri tiğlerinde kullanılamaz.
+# IOR operaörü list ve tuple gibi veri tiplerinde kullanılamaz.
 # Ancak set ve dict gibi veri tiplerinde kullanılabilir.
