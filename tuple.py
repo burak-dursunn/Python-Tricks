948a5491e191cd6b16734bf620405b3a9e8ac468
Burak Dursun <dursunburak046@gmail.com>
2024-11-03 02:09:41 +0300

    deneme

--- tuple.py
+++ tuple.py
@@ -1,7 +1,10 @@
 container = (1, 2, 3)
 print("Bu bir tuple ->", container)
+
 container_2 = 1, 2, 3
 print("Bu da bir tuple ->", container_2)
+
 container_3 = 1,  # Bu da bir tuple
 print("Conatainer 3 ->", type(container_3))
+
 container_4 = (1)  # Bu bir int, tuple değil
