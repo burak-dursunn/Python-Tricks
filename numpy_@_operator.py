d8f933d689926e5aeb968f036be85284eb9beb1f
Burak Dursun <dursunburak046@gmail.com>
2024-11-03 02:04:40 +0300

    Yorum satırı eklendi sadece

--- numpy_@_operator.py
+++ numpy_@_operator.py
@@ -7,4 +7,4 @@ b = np.array([[12, 10],
               [99, 24]])
 
 print(a @ b)  # Matrix multiplication. Yani matris çarpımı
-print(a.dot(b))  # Matrix multiplicaiton
+print(a.dot(b))  # Matrix multiplicaiton. Linear Algebra
